6994acab11f57a77469014c11f8afc7c09c8e4cb
nsohpeter <apiahpetertoch@gmail.com>
2023-10-12 21:48:28 -1100

    commiting

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
 import style from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={style.container}>this is the footer component</div>;
+  return (
+    <div className={style.container}>
+      <p> this is the footer component</p>
+      <hr className={style.footerhr} />
+      <p className="copy-right">
+        Made by SUPERCODE @ 2023- All Rights Reserved
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
